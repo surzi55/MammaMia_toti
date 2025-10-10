c2877978e9d74074c6b043ddefd9b900a403cc04
surzi55 <60424811+surzi55@users.noreply.github.com>
2025-10-10 08:22:00 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,6 +1,11 @@
-# static.py
-
 HTML = """
+Don’t care about them all
+Cause all I want is to be loved
+And all I care about is you
+You’re stuck on me like a tattoo
+No, I don’t care about the pain
+I’ll walk through fire and through rain
+"""
 <!DOCTYPE html>
 <html>
 <head>
